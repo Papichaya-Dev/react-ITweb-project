bb7511591b4d2d096d5ad7058f72f12ea5c0b5e8
unknown <lo0kpad@hotmail.com>
2020-06-08 23:17:24 +0700

    Finished Redux & Authentication

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import classnames from 'classnames';
+import { connect } from 'react-redux';
+import { registerUser } from '../../actions/authActions';
 
 class Register extends Component {
     constructor(){
@@ -16,6 +19,18 @@ class Register extends Component {
         this.onChange = this.onChange.bind(this);
         this.onSummit = this.onSummit.bind(this);
     }
+    componentDidMount(){
+      if(this.props.auth.isAuthenticated){
+        this.props.history.push('/dashboard');
+      }
+    }
+    
+
+    componentWillReceiveProps(nextProps){
+      if(nextProps.errors){
+        this.setState({errors: nextProps.errors});
+      }
+    }
     
     onChange(e) {
         this.setState({[e.target.name]: e.target.value});
@@ -30,18 +45,24 @@ class Register extends Component {
             password: this.state.password,
             password2:this.state.password2
         };
-        axios.post('/api/users/register',newUser)
-            .then(res => console.log(res.data))
-            .catch(err => this.setState({errors:err.response.data}));
+
+        this.props.registerUser(newUser,this.props.history);
+        // axios
+        //     .post('/api/users/register',newUser)
+        //     .then(res => console.log(res.data))
+        //     .catch(err => this.setState({errors:err.response.data}));
     }
 
     render() {
 
         const { errors } = this.state;
 
-        return (
-            <div className="register">
-    <div className="container">
+  
+
+    return (
+      <div className="register">
+
+      <div className="container">
       <div className="row">
         <div className="col-md-8 m-auto">
           <h1 className="display-4 text-center">Sign Up</h1>
@@ -101,4 +122,16 @@ class Register extends Component {
         )
     }
 }
-export default Register;
+
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  erorrs: state.errors
+});
+
+export default connect(mapStateToProps ,{ registerUser})(withRouter(Register));
